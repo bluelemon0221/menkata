2389d40e2aa1062b13651aa62088f97322f33a5f
bluelemon0221 <bluelemon5422@gmail.com>
2023-11-21 00:05:16 +0900

    全画面表示ができたけどやはり扱いづらい FullCalendarにする方が良いのか

--- src/app/daily/components/Calendar.tsx
+++ src/app/daily/components/Calendar.tsx
@@ -3,6 +3,7 @@ import { DateCalendar, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import React from "react";
 import { styled } from "@mui/system";
+import { Dayjs } from "dayjs";
 
 const FullScreenCalendar = styled(DateCalendar)({
   width: "100vw",
@@ -15,16 +16,28 @@ const FullScreenCalendar = styled(DateCalendar)({
     overflow: "visible",
   },
   ".MuiPickersDay-root": {
-    fontSize: "5rem",
-    height: "70px",
-    width: "70px",
+    fontSize: "7rem",
+    height: "120px",
+    width: "160px",
+  },
+  ".MuiDayCalendar-weekDayLabel": {
+    fontSize: "7rem",
+    height: "120px",
+    width: "160px",
   },
 });
+
+const japaneseDayFormatter = (day: string, date: Dayjs) => {
+  const japaneseWeekdays = ["日", "月", "火", "水", "木", "金", "土"];
+  const dayIndex = date.day();
+
+  return japaneseWeekdays[dayIndex];
+};
 
 export const Calendar = () => {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <FullScreenCalendar readOnly />
+      <FullScreenCalendar dayOfWeekFormatter={japaneseDayFormatter as any} />
     </LocalizationProvider>
   );
 };
